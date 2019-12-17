ed6b2d9339c9eeea2103ce99ec7ad8693c1240ae
Bitagi97 <bitagi1997@gmail.com>
2019-12-18 00:51:45 +0700

    add ORMs

--- models.py
+++ models.py
@@ -2,16 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Users(db.Model):
-    __tablename__ = "users"
+class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, nullable=False)
-    username = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False, unique = True)
+    username = db.Column(db.String, nullable=False, unique = True)
     password = db.Column(db.String, nullable=False)
 
 
-class PostBlogs(db.Model):
-    __tablename__ = "postblogs"
+class PostBlog(db.Model):
+    __tablename__ = "postblog"
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
--- run.py
+++ run.py
@@ -1,11 +1,35 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import scoped_session, sessionmaker
+
 from flask import Flask, render_template, request, redirect
+from models import *
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres@localhost:5555/myblog?password=123456789"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+
 
 @app.route('/', methods = ['GET', 'POST'])
 def helloWorld():
     if request.method == "POST":
-        print(request.form.get("formID"))
+        if(request.form.get("formID") == "formSignUp"):
+            user = User(email = request.form.get("email"), username = request.form.get("username"), password = request.form.get("password"))
+            db.session.add(user)
+            try:
+                db.session.commit()
+            except IntegrityError as err:
+                db.session.rollback()
+                numemail = User.query.filter_by(email = request.form.get("email")).count()
+                numusername = User.query.filter_by(username = request.form.get("username")).count()
+                print(numemail)
+                print(numusername)
+                
        
 
     return render_template('index.html')
