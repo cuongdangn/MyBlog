263aa42d088e4a641f654eaae4763580002df99e
Bitagi97 <bitagi1997@gmail.com>
2020-02-22 21:56:36 +0700

    .

--- createDataBase.py
+++ createDataBase.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 from models import *
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres@localhost:5432/?password=c_hFbTvTcJFwQw"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres@localhost:5555/?password=123456789"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
--- models.py
+++ models.py
@@ -14,7 +14,7 @@ class PostBlog(db.Model):
     __tablename__ = "postblog"
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), nullable=False)
     title = db.Column(db.String, nullable = False)
     time = db.Column(db.Date, nullable = False)
 
@@ -22,11 +22,11 @@ class Comment(db.Model):
     __tablename__  = "comment"
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String, nullable  = False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.user_id"), nullable=False)
     post_id = db.Column(db.Integer, db.ForeignKey("postblog.id"), nullable=False)
 
-class Like(db.Model):
-    __tablename__ = "like"
-    content = db.Column(db.String, nullable  = False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey("postblog.id"), nullable=False)
+# class Like(db.Model):
+#     __tablename__ = "like"
+#     content = db.Column(db.String, nullable  = False)
+#     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+#     post_id = db.Column(db.Integer, db.ForeignKey("postblog.id"), nullable=False)
