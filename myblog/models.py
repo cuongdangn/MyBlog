fd2aff3862a3f4571a946f36b45141d493c0a301
cuongdangn <cuongdangngoc97@gmail.com>
2020-05-06 16:18:17 +0700

    add reset password

--- myblog/models.py
+++ myblog/models.py
@@ -1,6 +1,8 @@
-from myblog import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from myblog import db, login_manager, app
 from flask_login import UserMixin
 import datetime
+from flask_mail import Message
 
 
 @login_manager.user_loader
@@ -14,6 +16,19 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String, nullable=False, unique = True)
     password = db.Column(db.String, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+
+    def get_reset_token(self, expires_sec = 600):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
